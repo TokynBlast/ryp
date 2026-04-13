22114026cc01b97d081bbef10ff190080ac9ca59
RuriMei <tokynblast@gmail.com>
2026-04-13 09:54:16 -0700

    Fix size

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -87,7 +87,7 @@ fn draw_terminal(f: &mut ratatui::Frame, app: &App, area: ratatui::layout::Rect)
     let popup_layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Length(vertical_margin),
+            Constraint::Length(vertical_margin - 3),
             Constraint::Min(0),
             Constraint::Length(vertical_margin),
         ])
