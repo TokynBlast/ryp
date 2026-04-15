144aee301f6786bc876a51a0b6c9bb878d39e85f
RuriMei <tokynblast@gmail.com>
2026-04-15 08:11:36 -0700

    Comment default_value

--- src/plugin/lua_integrate/settings.rs
+++ src/plugin/lua_integrate/settings.rs
@@ -1,4 +1,4 @@
-fn add_setting(name: String, default_value: ) {
+fn add_setting(name: String, /*=default_value:*/) {
   todo!("Both add default value, which can be any type,\nand implement actually adding it to the list of settings");
 }
 
