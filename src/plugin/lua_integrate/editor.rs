f3f0da19d8566f311c914db72e2716012855b71c
RuriMei <tokynblast@gmail.com>
2026-04-15 08:28:03 -0700

    Add editor functions

---
+++ src/plugin/lua_integrate/editor.rs
@@ -0,0 +1,15 @@
+fn get_char_at(x: usize, y: usize) {
+  todo!("Implement getting char in editor; plugin/lua_integrate/editor.rs");
+}
+
+fn get_line_at(row: usize) {
+  todo!("Implement getting line in editor; plugin/lua_integrate/editor.rs");
+}
+
+fn set_char_at(char_to_place: char, x: usize, y: usize) {
+  todo!("Implement setting a char at specific place; plugin/lua_integrate/editor.rs")
+}
+
+fn insert_char_at(char_to_place: char, x: usize, y: usize) {
+  todo!("Implement inserting a char at specific place; plugin/lua_integrate/editor.rs")
+}
