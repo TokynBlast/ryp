3e8bd62c845f2a0df1a7cab9927db0ef0415a4d4
RuriMei <tokynblast@gmail.com>
2026-04-17 15:01:53 -0700

    Remove StrictTable

--- src/plugin/policy.rs
+++ src/plugin/policy.rs
@@ -43,27 +43,5 @@ pub fn apply_restrictions(lua: &Lua, tx: &crossbeam::channel::Sender<crate::plug
     // Help to prevent version specific exploits
     globals.set("_VERSION", Nil)?;
 
-    // Fail on value not existing in table / global variables
-    let strict_index = lua.create_function(|_, (_table, key): (Value, String)| {
-        Err::<Value, _>(Error::RuntimeError(format!(
-            "RuntimeError: Variable or Field '{}' does not exist.",
-            key
-        )))
-    })?;
-
-    // Apply global protection
-    let global_mt = lua.create_table()?;
-    global_mt.set("__index", strict_index.clone())?;
-    let _ = globals.set_metatable(Some(global_mt));
-
-    // Users use this to make their own tables error on missing data
-    let create_strict_table = lua.create_function(move |lua, initial_data: Table| {
-        let mt = lua.create_table()?;
-        mt.set("__index", strict_index.clone())?;
-        let _ = initial_data.set_metatable(Some(mt));
-        Ok(initial_data)
-    })?;
-    globals.set("StrictTable", create_strict_table)?;
-
     Ok(())
 }
