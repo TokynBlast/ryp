e70e28c083123a5c27325c9757e7a06555dee36f
RuriMei <tokynblast@gmail.com>
2026-04-17 15:06:18 -0700

    Remove comment

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -176,7 +176,6 @@ fn draw_debug(f: &mut ratatui::Frame, app: &App, area: ratatui::layout::Rect) {
   let mut content: Vec<Line> = Vec::new();
 
   for log in &app.debug_logs {
-      // We can add a little timestamp or prefix if we want
       content.push(Line::from(vec![
           // Both because it looks cool, and to see where each log starts :)
           Span::styled(" λ ", Style::default().fg(Color::Gray)),
