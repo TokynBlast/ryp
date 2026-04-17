15c5666781474a70f271b1fd76efd90743832171
RuriMei <tokynblast@gmail.com>
2026-04-17 11:45:25 -0700

    Change how settings are stored

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,43 +4,19 @@ use indexmap::{IndexMap};
 pub type Config = IndexMap<String, Value>;
 
 pub fn default() -> Config {
-    let mut c = IndexMap::new();
-
-    c.insert("Tab Size".to_string(), json!(4));
-
-    c.insert("Auto Save".to_string(), json!(false));
-    c.insert("Time To Auto Save".to_string(), json!(30_000));
-
-    // Theme nest
-    c.insert("theme".to_string(), json!({
+    let default_json = json!({
+        "Tab Size": 4,
+        "Auto Save": false,
+        "Time To Auto Save": 30_000,
         "Tab BG Color": "#333333",
         "Active Tab BG Color": "#2E7D32",
         "Highlighting Theme": "base16-ocean.dark"
-    }));
+    });
 
-    c
-}
-
-// Like len, but also includes nests, ignoring actual nest
-pub fn nested_len(config: &Config) -> usize {
-    let mut total = 0;
-    for value in config.values() {
-        total += count_values(value);
-    }
-    total
-}
-
-fn count_values(value: &Value) -> usize {
-  match value {
-      // Get the insides of a nested object
-      Value::Object(map) => {
-          let mut sub_total = 0;
-          for v in map.values() {
-              sub_total += count_values(v);
-          }
-          sub_total
-      }
-
-      _ => 1,
-  }
+    // Convert the JSON into an object for IndexMap
+    default_json.as_object()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .collect()
 }
