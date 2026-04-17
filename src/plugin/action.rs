c2639768bea77b67140d13b7e37a54cc2040b9a4
RuriMei <tokynblast@gmail.com>
2026-04-17 11:23:16 -0700

    Change sender of setting get -> serde_json Value

--- src/plugin/action.rs
+++ src/plugin/action.rs
@@ -1,6 +1,6 @@
 pub enum PluginAction {
   MakeSetting { name: String, value: mlua::Value },
   InsertText { text: String, x: usize, y: usize },
-  GetSettingValue { name: String, tx_respond: oneshot::Sender<String> },
+  GetSettingValue { name: String, tx_respond: oneshot::Sender<serde_json::Value> },
   DebugLog { message: String },
 }
