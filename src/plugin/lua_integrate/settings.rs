ae11715fd234a216f04b0d58e4b84db613275a7b
TokynBlast <tokynblast@gmail.com>
2026-04-16 19:34:18 -0700

    Add inline to settings

--- src/plugin/lua_integrate/settings.rs
+++ src/plugin/lua_integrate/settings.rs
@@ -1,13 +1,15 @@
 use mlua;
 
-pub fn add_setting(name: mlua::String, default_value: mlua::Value) -> Result<(), mlua::Error>  {
+#[inline]
   todo!("Both add default value, which can be any type,\nand implement actually adding it to the list of settings");
 }
 
+#[inline]
 fn get_setting_value() {
   todo!("Add getting settings\nWe need a policy so as the user can choose to set the scope");
 }
 
+#[inline]
 fn set_setting_value() {
   todo!("Add value setting for Lua implemented settings");
 }
