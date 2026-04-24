9da3c9ea8d13bffa7076219d1d0b5d99fd014086
RuriMei <tokynblast@gmail.com>
2026-04-24 12:20:17 -0700

    Add more settings

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -10,7 +10,19 @@ pub fn default() -> Config {
         "Time To Auto Save": 30_000,
         "Tab BG Color": "#333333",
         "Active Tab BG Color": "#2E7D32",
-        "Highlighting Theme": "base16-ocean.dark"
+        "Highlighting Theme": "base16-ocean.dark",
+        "Search": "Ctrl+F",
+        "Help": "Ctrl+K",
+        "Sidebar Toggle": "Ctrl+B",
+        "Previous Sidebar Tab": "Ctrl+A",
+        "Next Sidebar Tab": "Ctrl+D",
+        "Refresh Git": "Ctrl+G",
+        "Close Tab": "Ctrl+A",
+        "Quit": "Ctrl+W",
+        "New File": "Ctrl+N",
+        "Save File": "Ctrl+S",
+        "Open Terminal": "Ctrl+T",
+        "Open Debug Console": "Ctrl+E",
     });
 
     // Convert the JSON into an object for IndexMap
