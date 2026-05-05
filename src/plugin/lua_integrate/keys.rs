3370de81d02a18a37c52107bbfd022a29538e265
RuriMei <tokynblast@gmail.com>
2026-05-05 13:09:52 -0700

    Simplify key press detection

--- src/plugin/lua_integrate/keys.rs
+++ src/plugin/lua_integrate/keys.rs
@@ -3,43 +3,30 @@ use triomphe::Arc;
 use mlua::{self, LuaSerdeExt};
 use crate::plugin::action::{PluginAction, CharResponder};
 
-fn get_key_press(lua: &mlua::Lua, tx: &crossbeam_channel::Sender<PluginAction>, get_table: &mlua::Table) -> Result<(), mlua::Error> {
+pub fn integrate_keys(lua: &mlua::Lua, tx: &crossbeam_channel::Sender<PluginAction>) -> Result<(), mlua::Error> {
+    let tx_clone = tx.clone();
+
     let responder = Arc::new(CharResponder {
         c: Mutex::new(None),
         signal: Condvar::new(),
     });
-    let responder_clone = responder.clone();
-    let tx_clone = tx.clone();
-    get_table.set("key",
-        lua.create_function(move |lua, ()| {
-              let _ = tx_clone.send(PluginAction::GetKeyPress { responder: responder_clone.clone() });
 
-              let mut lock = responder_clone.c.lock();
-              if lock.is_none() {
-                  responder_clone.signal.wait(&mut lock);
-              }
+    let key_fn = lua.create_function(move |lua, ()| {
+        let responder_clone = responder.clone();
+        let _ = tx_clone.send(PluginAction::GetKeyPress {
+            responder: responder_clone.clone()
+        });
 
-              let info = lock.take();
-              lua.to_value(&info)
-        })?
-    )?;
-    Ok(())
-}
+        let mut lock = responder_clone.c.lock();
+        if lock.is_none() {
+            responder_clone.signal.wait(&mut lock);
+        }
 
-// wiring to expose register_shortcut to Lua
-pub fn integrate_keys(lua: &mlua::Lua, tx: &crossbeam_channel::Sender<PluginAction>) -> Result<(), mlua::Error> {
-    let key_table = lua.create_table()?;
-    get_key_press(lua, tx, &key_table)?;
-    let proxy = lua.create_table()?;
-    let metatable = lua.create_table()?;
+        let info = lock.take();
+        lua.to_value(&info)
+    })?;
 
-    let internal_editor = key_table.clone();
-    metatable.set("__index", lua.create_function(move |_, (_, key): (mlua::Value, String)| {
-        internal_editor.get::<mlua::Value>(key)
-    })?)?;
+    lua.globals().set("key", key_fn)?;
 
-    proxy.set_metatable(Some(metatable))?;
-
-    lua.globals().set("editor", proxy)?;
     Ok(())
 }
