330d80a037d6a3874f6b7a4c949c8b1242901460
RuriMei <tokynblast@gmail.com>
2026-05-06 08:04:12 -0700

    Update sidebar input

--- src/plugin/lua_integrate/sidebar.rs
+++ src/plugin/lua_integrate/sidebar.rs
@@ -1,3 +1,5 @@
-fn add_sidebar_button(icon: String) {
+use crate::plugin::action::PluginAction;
+
+fn add_sidebar_button(lua: &mlua::Lua, tx: &crossbeam_channel::Sender<PluginAction>, set_table: &mlua::Table) -> Result<(), mlua::Error> {
     todo!("Implement Lua sidebar adding and icon setting");
 }
