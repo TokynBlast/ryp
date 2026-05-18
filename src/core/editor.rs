9bcbc4fbd7c050011168576bd3a31a7f2865c96e
RuriMei <tokynblast@gmail.com>
2026-05-18 11:12:25 -0700

    Add a TODO

--- src/core/editor.rs
+++ src/core/editor.rs
@@ -80,6 +80,7 @@ impl Editor {
             self.is_diff = false;
             self.lang = CompactString::const_new(
                 if let Some(path) = &self.filepath {
+                // TODO: Implement Naive Bayes algorithm for file detection, rather than rely purely on file ending
                 match path.extension()
                     .and_then(|e| e.to_str())
                     .or_else(|| path.file_name().and_then(|n| n.to_str()))
