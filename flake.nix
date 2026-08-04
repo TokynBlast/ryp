564f27c2dc16aa0c749c7f3489bc0ebc1c736e5d
sophie <sophie@qwq.sh>
2026-08-04 09:04:57 +0000

    fix the flake

--- flake.nix
+++ flake.nix
@@ -6,16 +6,39 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        nerdFontTar = pkgs.fetchurl {
+          url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.tar.xz";
+          hash = "sha256-1yvdg/AqkHHUuwrNOKN/5oeke9KRieg47fhH7Tm4fhA=";
+        };
+        cargoToml = fromTOML (builtins.readFile ./Cargo.toml);
         version = cargoToml.package.version;
       in
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
+          postPatch = ''
+            perl -0777 -pi -e '
+              s/let response = ureq::get\(url\)\s*\.call\(\)\s*\.expect\([^;]*\);/
+              let font_tar_path = std::env::var("RYP_FONT_TAR").expect("RYP_FONT_TAR not set");/s
+            ' build.rs
+
+            perl -0777 -pi -e '
+              s/let mut body = response\.into_body\(\);\s*let xz_reader = xz2::read::XzDecoder::new\(body\.as_reader\(\)\);/
+              let font_tar_file = std::fs::File::open(\&font_tar_path).expect("Failed to open vendored font tarball");
+              let xz_reader = xz2::read::XzDecoder::new(font_tar_file);/s
+            ' build.rs
+          '';
+          RYP_FONT_TAR = nerdFontTar;
           pname = cargoToml.package.name;
           inherit version;
 
@@ -33,13 +56,14 @@
 
           nativeBuildInputs = [
             pkgs.pkg-config
+            pkgs.perl
           ];
 
           meta = with pkgs.lib; {
             description = cargoToml.package.description or "Rust application";
-            license = licenses.mit; # change if needed
+            license = licenses.bsd2;
             platforms = platforms.unix;
-            mainProgram = cargoToml.package.name;
+            ryp = cargoToml.package.name;
           };
         };
 
@@ -57,7 +81,8 @@
             pkgs.libssh2
           ];
         };
-      })
+      }
+    )
     // {
       overlays.default = final: prev: {
         ryp = self.packages.${final.system}.default;
